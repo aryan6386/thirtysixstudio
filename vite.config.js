adb42e50d8eab522f814f56a1c7189bef039bdf7
Aryan Gupta <aryanguptagola@gmail.com>
2024-11-29 19:05:31 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: 'thirtysixstudio', // Replace 'your-repository-name' with your GitHub repo name
+});
